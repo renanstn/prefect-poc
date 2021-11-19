5c459cb13e9ec198d37079f1d74f54a351ea38b7
Renan Santana <renan_ota@hotmail.com>
2021-11-19 17:40:49 -0300

    adiciona --noinput ao comando migrate

--- apis/core_app/setup_dev.sh
+++ apis/core_app/setup_dev.sh
@@ -3,7 +3,7 @@
 black .
 python manage.py collectstatic --noinput;
 python manage.py makemigrations;
-python manage.py migrate;
+python manage.py migrate --noinput;
 python manage.py createadmin;
 
 uvicorn core_app.asgi:application --host 0.0.0.0 --port 80 --reload
--- apis/core_app/setup_prod.sh
+++ apis/core_app/setup_prod.sh
@@ -2,6 +2,6 @@
 
 python manage.py collectstatic --noinput;
 python manage.py makemigrations;
-python manage.py migrate;
+python manage.py migrate --noinput --check;
 
 uvicorn core_app.asgi:application --host 0.0.0.0 --port 80
